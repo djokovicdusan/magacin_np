7fcc962eacaac13a70b27772ce07d5f563a2a63e
djokovicdusan <ddjokovic9@gmail.com>
2022-03-12 13:17:49 +0100

    Implementiran interfejs u klasi Magacin

--- src/model/Magacin.java
+++ src/model/Magacin.java
@@ -1,5 +1,30 @@
 package model;
 
-public class Magacin {
+import java.util.LinkedList;
+
+import interfaces.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs{
+	public LinkedList<Artikal> artikli= new LinkedList<Artikal>();
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 
 }
