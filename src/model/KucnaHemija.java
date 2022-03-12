0352a683b1001d0b7470f6908bbe37d0d27f3c9f
djokovicdusan <ddjokovic9@gmail.com>
2022-03-12 12:33:33 +0100

    Inicijalni commit, napravljene sve domenske klase

---
+++ src/model/KucnaHemija.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.Date;
+
+public class KucnaHemija extends Artikal{
+
+	public Date rokTrajanja;
+
+	public Date getRokTrajanja() {
+		return rokTrajanja;
+	}
+
+	public void setRokTrajanja(Date rokTrajanja) {
+		//validacija
+		this.rokTrajanja = rokTrajanja;
+	}
+	
+}
